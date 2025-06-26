4729889667fe483b94bd56e4d012c5a51f8b6d49
rockhero1234 <149141736+rockhero1234@users.noreply.github.com>
2025-06-26 12:46:55 +0530

    Update bingedProvider.kt

--- BingedReview/src/main/kotlin/binged/bingedProvider.kt
+++ BingedReview/src/main/kotlin/binged/bingedProvider.kt
@@ -57,7 +57,7 @@ class BingedProvider : MainAPI() {
                 url = entry["link"].toString(),
                 type = TvType.Movie
             ) {
-                this.posterUrl = entry["small-image"].toString()
+                this.posterUrl = entry["big-image"].toString()
             }
         } ?: emptyList()
     }
